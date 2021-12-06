2a9c5165673629d7d89f1048d7c6d2501aab6577
Janriz Libres <leonmadrik11037@gmail.com>
2021-12-06 20:16:39 +0800

    added class wrapper to handle user input

--- src/main/App.java
+++ src/main/App.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class App {
     public static ProcessBuilder cls = new ProcessBuilder("cmd", "/c", "cls").inheritIO();
     public static Scanner input = new Scanner(System.in);
+    public static Input scan = new Input();
 
     public interface Function {
         public void solve() throws InterruptedException, IOException;
@@ -22,56 +23,29 @@ public class App {
 
         // Main loop (program flow)
         while (true) {
-            displayMenu();
-            int shape = promptUser();
+            int shape = scan.nextInt(
+                "Enter a choice: ",
+                """
+                Pick a shape
+                ------------------------------------
+                  2D Shapes            3D Shapes
+                ------------------------------------
+                1 - Circle           7  - Sphere
+                2 - Square           8  - Cube
+                3 - Rectangle        9  - Cuboid
+                4 - Triangle         10 - Cone
+                5 - Parallelogram    11 - Cylinder
+                6 - Trapezium
+                          0 - Exit Program
+                ------------------------------------
+                """,
+                0, 11
+            );
 
-            if (shape == -1)
+            if (shape == 0)
                 break;
 
-            shapes[shape].solve();
+            shapes[shape - 1].solve();
         }
-    }
-
-    private static void displayMenu() throws InterruptedException, IOException {
-        cls.start().waitFor(); // Clear screen
-
-        System.out.print(
-            """
-                        Pick a shape
-            ------------------------------------
-              2D Shapes            3D Shapes
-            ------------------------------------
-            1 - Circle           7  - Sphere
-            2 - Square           8  - Cube
-            3 - Rectangle        9  - Cuboid
-            4 - Triangle         10 - Cone
-            5 - Parallelogram    11 - Cylinder
-            6 - Trapezium
-                      0 - Exit Program
-            ------------------------------------
-            """
-        );
-    }
-
-    private static int promptUser() throws InterruptedException, IOException {
-        System.out.print("Enter a choice: ");
-
-        int shape = 0;
-        while (true) {
-            if (input.hasNextInt()) {
-                shape = input.nextInt() - 1;
-                
-                if (shape >= -1 && shape <= 10) {
-                    input = new Scanner(System.in);
-                    break;
-                }
-            }
-
-            input = new Scanner(System.in);
-            displayMenu();
-            System.out.print("Invalid input! Enter valid choice: ");
-        }
-
-        return shape;
     }
 }
